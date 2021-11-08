6e632e7a4bcf69fc0d563dbc25600584ca690c78
TheAncientOwl <dele.alex.o.o@gmail.com>
2021-11-08 11:37:24 +0200

    update

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -3,6 +3,9 @@ Set-PoshPrompt -Theme the-ancient-owl
 
 Import-Module PSColor
 
+Set-PSReadLineOption -PredictionSource History
+
 Set-Alias -Name touch -Value New-Item
 
 Set-Alias -Name python -Value py
+Set-Alias -Name winget -Value winget.exe

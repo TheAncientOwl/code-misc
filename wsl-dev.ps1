00d5a9786f6746a81ec6669cf8ca8fe0ca2eac28
TheAncientOwl <dele.alex.o.o@gmail.com>
2021-11-11 14:16:39 +0200

    updated.

--- wsl-dev.ps1
+++
@@ -1,7 +0,0 @@
-if (!(Get-Process -Name spotify)) {
-  Start-Process -FilePath "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Spotify.lnk"
-}
-
-wsl code ~/dev/pensiune-leresti/
-
-Start-Process -FilePath "C:\Program Files\Google\Chrome\Application\chrome.exe" -ArgumentList "http://localhost:3000/"
